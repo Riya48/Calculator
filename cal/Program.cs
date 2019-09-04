8b28d305ee1532d3608eceb6d322cf90b27834b1
Riya Shree <shreeriya9@gmail.com>
2019-09-04 12:15:34 +0530

    Initial commit

---
+++ cal/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace cal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            string operand;
+            ConsoleKeyInfo status;
+            float answer;
+
+            while (true)
+            {
+                Console.WriteLine("---------------------CALCULATOR----------------------");
+                Console.Write("Enter 1st number: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter 2nd number: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\t+ : Add");
+                Console.WriteLine("\t- : Subtract");
+                Console.WriteLine("\t* : Multiply");
+                Console.WriteLine("\t/ : Divide");
+                Console.Write("Select an operand (+, -, /, *): ");
+                operand = Console.ReadLine();
+
+                switch (operand)
+                {
+                    case "-":
+                        answer = num1 - num2;
+                        break;
+                    case "+":
+                        answer = num1 + num2;
+                        break;
+                    case "/":
+                        answer = num1 / num2;
+                        break;
+                    case "*":
+                        answer = num1 * num2;
+                        break;
+                    default:
+                        answer = 0;
+                        break;
+                }
+
+                Console.WriteLine(num1.ToString() + " " + operand + " " + num2.ToString() + " = " + answer.ToString());
+                Console.WriteLine("\n\n Do You Want To Break Press(Y/y) or Press any Key to Continue:");
+                Console.WriteLine("-----------------------------------------------------");
+                status = Console.ReadKey();
+                if (status.Key == ConsoleKey.Y)
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
+    
+
